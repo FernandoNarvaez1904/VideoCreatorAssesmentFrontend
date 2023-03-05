68e6b2e11a5bd85647acc271c0a8d96ebd3351cd
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-05 15:57:53 -0600

    Added useForm

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,31 @@
 import {Anchor, Button, Paper, PasswordInput, Text, TextInput, Title,} from '@mantine/core';
 import {useLoginStyles} from "./Register.styles";
 import {Link} from "react-router-dom";
+import {useForm} from "@mantine/form";
 
+interface RegisterForm {
+  username?: string;
+  firstName?: string;
+  lastName?: string;
+  password?: string;
+  email?: string
+}
 
 export function Register() {
   const {classes} = useLoginStyles();
+
+
+  const form = useForm<RegisterForm>({
+    initialValues: {
+      username: '',
+      email: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+    },
+  });
+
+
   return (
     <div className={classes.wrapper}>
       <Paper className={classes.form} radius={0} p={30}>
@@ -12,16 +33,22 @@ export function Register() {
           Register To Creator Platform!
         </Title>
 
-        <TextInput label="Username" placeholder="some_username" mt="md" size="md" required/>
-        <TextInput label="First Name" placeholder="John Marcus" mt="md" size="md" required/>
-        <TextInput label="Last Name" placeholder="Doe Smith" mt="md" size="md" required/>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput label="Username" placeholder="someUsername" mt="md" size="md"
+                     required {...form.getInputProps('username')}/>
+          <TextInput label="Email" type={"email"} placeholder="john.doe@gmail.com" mt="md" size="md"
+                     required {...form.getInputProps('email')}/>
+          <TextInput label="First Name" placeholder="John Marcus" mt="md" size="md"
+                     required {...form.getInputProps('firstName')}/>
+          <TextInput label="Last Name" placeholder="Doe Smith" mt="md" size="md"
+                     required {...form.getInputProps('lastName')}/>
+          <PasswordInput label="Password" placeholder="Your password" mt="md" size="md"
+                         required {...form.getInputProps('password')}/>
 
-
-        <PasswordInput label="Password" placeholder="Your password" mt="md" size="md" required/>
-
-        <Button fullWidth mt="xl" size="md">
-          Register
-        </Button>
+          <Button fullWidth mt="xl" size="md" type={"submit"}>
+            Register
+          </Button>
+        </form>
 
         <Text ta="center" mt="md">
           Already have an account?{' '}
