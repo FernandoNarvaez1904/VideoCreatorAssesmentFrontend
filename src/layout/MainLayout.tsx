8a473322e7c211992d515072adfb2c550949bfff
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-07 15:49:44 -0600

    Delete red border

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -6,7 +6,7 @@ function MainLayout() {
   return (
     <>
       <Navbar />
-      <Container sx={{ border: "1px solid red" }} size={"md"} mt={20} h={"88%"}>
+      <Container size={"md"} mt={20} h={"88%"}>
         <Outlet />
       </Container>
     </>
